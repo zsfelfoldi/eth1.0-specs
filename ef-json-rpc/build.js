343b75df2bdc6dd4cbd6d89778db82d3b3a0875c
Alita Moore <alita.moore805@gmail.com>
2021-05-21 21:53:45 -0500

    build workspace (can easily develop with open rpc preview now)

---
+++ ef-json-rpc/build.js
@@ -0,0 +1,17 @@
+const RefParser = require("@apidevtools/json-schema-ref-parser");
+const fs = require("fs");
+
+const _parse = async (_schema) => {
+  try {
+    let schema = await RefParser.dereference(_schema);
+    if (!fs.existsSync("./build")){
+      fs.mkdirSync("./build");
+  }
+    fs.writeFileSync("./build/openrpc.json", JSON.stringify(schema))
+  }
+  catch(err) {
+    console.error(err);
+  }
+}
+
+_parse(require("./index.json"))
